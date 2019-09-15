5520428046c0b4ae274cc402060b01ce09268a63
Cornelia1991 <Cornelia1991@users.noreply.github.com>
2019-09-15 15:59:19 -0400

    Ceeating and Using Methods

---
+++ IntroductionToMethodsLecture/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroductionToMethodsLecture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             
+            returnType Name (perameters)
+            {
+                //do this
+            }
+
+             */
+
+            // methods are the same thing as functions in other lanuages
+
+            Console.WriteLine("You're learning how to program!");
+            PrintCongrats();
+
+            Console.WriteLine("You are doing well!");
+            PrintCongrats();
+
+            Console.WriteLine("You are going to be rich!");
+            PrintCongrats();
+
+            Console.ReadKey();
+        }
+
+        //basic method does not return or pass data (void with no perameters)
+        static void PrintCongrats()
+        {
+            Console.WriteLine("Congratulations!!!!!");
+        }
+    }
+}
---
+++ ReturningPassingDataLecture/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReturningPassingDataLecture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //return string literal
+            Console.WriteLine("You're learning how to program");
+            Console.WriteLine(ReturnCongratMessage());
+
+            //data passed into the method
+            int result = NumberSquared(12);
+            Console.WriteLine(result); //returns 144
+
+            //passing in mutilple data
+            Console.WriteLine(MuiltipleData(7,10));
+
+
+            Console.ReadKey();
+        }
+        // return string literal
+        static string ReturnCongratMessage()
+        {
+            return "Congratulations";
+        }
+
+        //data passed into the method (returns and expects and int value)
+        static int NumberSquared(int x)
+        {
+            return x * x;
+        }
+
+        //passing in mutilple data
+        static int MuiltipleData(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
