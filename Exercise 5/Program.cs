e43b4ecd562da6e34e376a2607a296a8e088c088
Cornelia1991 <Cornelia1991@users.noreply.github.com>
2019-09-15 19:35:56 -0400

    Exercise 5 - Simple Method

---
+++ Exercise 5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a name:  ");
+            
+            Console.WriteLine("Hello"+ NameEntered());
+
+            Console.Write("Enter another name:  ");
+            Console.WriteLine("Hello " + NameEntered());
+
+            Console.Write("Enter a third name:  ");
+            Console.WriteLine("Hello " + NameEntered() + " How are you doing today?");
+
+            Console.ReadKey();
+        }
+
+        static string NameEntered()
+        {
+            return Console.ReadLine();  
+        }
+    }
+}
