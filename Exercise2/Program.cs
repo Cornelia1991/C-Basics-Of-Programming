6cf76b3507a959875c11b21997179dee5f638ced
Cornelia1991 <Cornelia1991@users.noreply.github.com>
2019-09-15 13:07:25 -0400

    Exercise 3 Shopping Decision

---
+++ Exercise2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string onSale;
+            double money;
+
+            Console.Write("Are drinks 2 For 1? ");
+            onSale = Console.ReadLine().ToLower();
+
+
+            //response to on sale
+            if (onSale == "yes" || onSale == "no")
+            {
+                Console.Write("How much money do you have? ");
+                money = Convert.ToDouble(Console.ReadLine());
+
+                if (money >= 10)
+                {
+                    Console.WriteLine("Go Shopping");
+                }
+                else
+                {
+                    Console.WriteLine("Stay Home");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid Entry");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
