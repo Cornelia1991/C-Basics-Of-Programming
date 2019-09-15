d5621b8b0bbe04a95a4ca5da7874faddec1251a9
Cornelia1991 <Cornelia1991@users.noreply.github.com>
2019-09-15 13:56:03 -0400

    Pratice with loop and condition statements

---
+++ Excercise 3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pizzaQuestion;
+
+            do { 
+                Console.WriteLine("Do you like pizza? (yes/no) ");
+                pizzaQuestion = Console.ReadLine().ToLower();
+
+                if (pizzaQuestion == "yes")
+                {
+                    Console.WriteLine("You Like Pizza!");
+                    break;
+                }
+                else if (pizzaQuestion == "no")
+                {
+                    Console.WriteLine("I feel sorry for you");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Please Enter yes/no");
+                }
+            }
+            while (pizzaQuestion != "yes" || pizzaQuestion != "no");
+            Console.ReadKey();
+        }
+    }
+}
