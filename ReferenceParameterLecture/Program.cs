58455dc4e3a682daa9100ec5e3042fde5a2b7c57
Cornelia1991 <Cornelia1991@users.noreply.github.com>
2019-09-15 19:08:26 -0400

    Reference Parameters

---
+++ ReferenceParameterLecture/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReferenceParameterLecture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 5; //to use the ref keyword variable must be initalizes
+            int w;//using out keyout valus does not need to be initailized
+
+            ChangeNumberRef(ref x); //will make x = 1 (using ref keyword)
+
+            ChangeNumberOut(out w);//will make x = 0 (using out keyword)
+
+            Console.WriteLine(x);
+            Console.WriteLine(w);
+
+            Console.ReadKey();
+        }
+
+        static void ChangeNumberRef(ref int y)
+        {
+            y = 1;
+        }
+
+        static void ChangeNumberOut(out int y)
+        {
+            y = 0;
+        }
+    }
+}
