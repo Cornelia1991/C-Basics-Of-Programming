64f4683ddcc12336126e5befbaabc76479d62ae9
Cornelia1991 <Cornelia1991@users.noreply.github.com>
2019-09-16 19:57:15 -0400

    Array Lecture

---
+++ ArraysLecture/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysLecture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myNumberArray = {2, 5, 7};
+            int[] myNumberArray2 = new int[3]; // no value specified, default value 0
+
+            Console.WriteLine(myNumberArray[0]);
+
+            myNumberArray[1] = 12; //replace 5 with 12
+
+            for (int i = 0; i < myNumberArray.Length; i++)
+            {
+                Console.WriteLine(myNumberArray[i]);
+            }
+
+            foreach(int number in myNumberArray)
+            {
+                Console.WriteLine(number);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
