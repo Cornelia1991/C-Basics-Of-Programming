f0523387c702b6236e6169103df93c7a5a744ae4
Cornelia1991 <Cornelia1991@users.noreply.github.com>
2019-09-16 20:25:18 -0400

    Values vs References Lecture

---
+++ ValueVsReferenceTypes/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValueVsReferenceTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray1 = {1,2,3,};
+            int[] myArray2;
+
+            myArray2 = myArray1; //does not copy the array of myArray1 only the address of where it is stored
+                                // Now there are not 2 different arrays
+                                // both arrays are referenicing the same array
+
+            //showning that the two arrays are the same
+            Console.WriteLine(myArray2[0]); //output 1
+            Console.WriteLine(myArray2[1]); //output 2
+            Console.WriteLine(myArray2[2]); //output 3
+            myArray2[0] = 7;
+            Console.WriteLine(myArray1[0]); //output 7
+
+            //set value back to 1
+            myArray1[0] = 1;
+
+            //shows how to clone array
+            myArray2 = (int[])myArray1.Clone();
+            //shows that the arrays are now referencing to differnt arrays
+            myArray2[0] = 7;
+            Console.WriteLine(myArray1[0]); //output 1
+        }
+    }
+}
