2693719e316ca49ad1e7970eff34c327761ba868
Cornelia1991 <Cornelia1991@users.noreply.github.com>
2019-09-18 18:14:06 -0400

    OOP Lecture 1

---
+++ IntoToOOPLecture/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntoToOOPLecture
+{
+    class Lamp
+    {
+        //fields of lamp class
+        public string color;
+        public bool isOn;
+
+        //methods of lamp class
+        public void TurnOn()
+        {
+            isOn = true;
+        }
+        public void TurnOff()
+        {
+            isOn = false;
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiate an object / creating and instance of an object
+            Lamp Lamp1 = new Lamp();
+            Lamp Lamp2 = new Lamp();
+            Lamp Lamp3 = new Lamp();
+
+            Lamp1.color = "green";
+            Lamp1.isOn = true;
+
+            Lamp2.color = "blue";
+            Lamp2.isOn = false;
+
+            Lamp3.color = "red";
+            Lamp3.isOn = true;
+
+            Console.WriteLine(Lamp1.isOn);//output true
+
+            Lamp1.TurnOff();
+            Console.WriteLine(Lamp1.isOn);//output true
+
+            Console.ReadKey();
+        }
+    }
+}
