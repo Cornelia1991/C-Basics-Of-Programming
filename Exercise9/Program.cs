296798d9e34076952de8e29d0eeab4eb6c3f4ab2
Cornelia1991 <Cornelia1991@users.noreply.github.com>
2019-09-17 21:11:03 -0400

    Excercise 9 (to be continued)

---
+++ Exercise9/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise9
+{
+
+    enum direction {North, South, East, West};
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> direction = new List<string>();
+            string newInstruction;
+
+            do
+            {
+                Console.Write("Enter the next direction (n/s/e/w or 0 to finsh)");
+                newInstruction = Console.ReadLine();
+  
+
+                direction.Add(newInstruction);
+
+
+            } while (newInstruction != "0");
+
+            foreach (string newItem in direction)
+            {
+                Console.WriteLine(newItem);
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
