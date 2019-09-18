ffb95f92b30aa637d643b91b2e22e21d8f5e8b37
Cornelia1991 <Cornelia1991@users.noreply.github.com>
2019-09-18 17:47:25 -0400

    Answer Complete

--- Exercise9/Program.cs
+++ Exercise9/Program.cs
@@ -6,8 +6,9 @@ using System.Threading.Tasks;
 
 namespace Exercise9
 {
-
-    enum direction {North, South, East, West};
+    // ask user enter next direction n, s, e, w or 0 to exit
+    //if the input is invalid notify the user of an invalid entry
+    // Print out arrows for n, s, e, w
 
     class Program
     {
@@ -18,18 +19,51 @@ namespace Exercise9
 
             do
             {
-                Console.Write("Enter the next direction (n/s/e/w or 0 to finsh)");
+                Console.Write("Enter the next direction (n/s/e/w or 0 to finsh)  ");
                 newInstruction = Console.ReadLine();
-  
+                bool onlyValid = newInstruction == "n" || newInstruction == "s" || newInstruction == "e" || newInstruction == "w" || newInstruction == "0";
 
-                direction.Add(newInstruction);
+                if (!onlyValid)
+                {
+                    direction.Remove(newInstruction);
+                    Console.WriteLine("Invalid Entry");
+                }
+                else
+                {
+                    direction.Add(newInstruction);
+                }
+
+
+                switch(newInstruction)
+                {
+                    case "n":
+                        direction.Remove("n");
+                        direction.Add("^");
+                        break;
+                    case "s":
+                        direction.Remove("s");
+                        direction.Add("v");
+                        break;
+                    case "e":
+                        direction.Remove("e");
+                        direction.Add(">");
+                        break;
+                    case "w":
+                        direction.Remove("w");
+                        direction.Add("<");
+                        break;
+                    case "0":
+                        direction.Remove("0");
+                        direction.Add("Route Complete!");
+                        break;
+                }
 
 
             } while (newInstruction != "0");
 
             foreach (string newItem in direction)
             {
-                Console.WriteLine(newItem);
+                Console.WriteLine( newItem);
             }
 
 
