0916e2e912d7699adf3f00e99d8a2d7808a7589d
Cornelia1991 <Cornelia1991@users.noreply.github.com>
2019-09-20 19:03:14 -0400

    First Commit ATM Excercise Checking/Savings/Balance

---
+++ Exercise11/Checkings.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise11
+{
+    class Checkings
+    {
+        private string checkingMenuSelect;
+
+        public void CheckingsMenu()
+        {
+            do
+            {
+                Console.WriteLine("Checkings Account Menu");
+                Console.WriteLine("1 - Desposit");
+                Console.WriteLine("2 - Withdraw");
+                Console.WriteLine("3 - Check Balance");
+                Console.WriteLine("4 - Exit");
+                checkingMenuSelect = Console.ReadLine();
+
+                if (checkingMenuSelect == "1")
+                {
+                    CheckingsDeposit();
+                }
+
+            } while (checkingMenuSelect != "4");
+        }
+
+        public void CheckingsDeposit()
+        {
+            double depositAmount;
+
+            Console.Write("Enter the deposit amount: "); 
+            depositAmount = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"A total of ${depositAmount.ToString("F")} has been despoited into your account");
+        }
+    }
+}
---
+++ Exercise11/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mainMenuSelect;
+
+            Checkings checkings = new Checkings();
+            do
+            {
+                Console.WriteLine("Welcome Please Make A Selection");
+                Console.WriteLine("1 - Checking Account");
+                Console.WriteLine("2 - Saving Account");
+                Console.WriteLine("3 - Check Balance");
+                Console.WriteLine("4 - Exit");
+                mainMenuSelect = Console.ReadLine();
+
+                if (mainMenuSelect == "1")
+                {
+                    checkings.CheckingsMenu();
+                }
+            } while (mainMenuSelect != "4");
+
+
+           
+        }
+    }
+}
