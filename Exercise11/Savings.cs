1608ce77468f3dde765f12a27bc7c7bda61e6702
Cornelia1991 <Cornelia1991@users.noreply.github.com>
2019-09-20 20:05:54 -0400

    Savvings first commit Savings inheritances from Checkings

---
+++ Exercise11/Savings.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Exercise11
+{
+    public class Savings : Checkings
+    {
+        private double interest; //10 percent
+
+        public Savings() 
+        {
+            
+        }
+
+        public override void Deposit()
+        {
+            Console.Write("Enter the deposit amount: ");
+            deposit = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"A total of ${deposit.ToString("F")} has been despoited into your account");
+            currentBalance += deposit;
+            interest = currentBalance * 0.10;
+            Console.WriteLine($"You earned ${interest.ToString("F")} in interest");
+            currentBalance += interest;
+            Console.WriteLine($"Total Balance: ${currentBalance.ToString("F")}");
+        }
+
+    }
+}
