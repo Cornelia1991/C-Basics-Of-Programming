7927c9b8a891707d80f534e0a93057c30bec18b8
Cornelia1991 <Cornelia1991@users.noreply.github.com>
2019-09-21 10:24:45 -0400

    Lecture Error Handling: Try Catch

---
+++ ErrorHandlingLecture2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErrorHandlingLecture2
+{
+    class Program
+    {
+        //Try Catch 
+        static void Main(string[] args)
+        {
+            int[] myArray = {1, 2, 3};
+            int selection;
+            bool error;
+
+            do
+            {
+                error = false;
+                try
+                {
+                    Console.Write("Enter the array index of the element you with to view: ");
+                    selection = Int32.Parse(Console.ReadLine());
+                    Console.WriteLine(myArray[selection]);
+                }
+                catch (Exception ex)
+                {
+                    error = true;
+                    Console.WriteLine(ex.Message + "Please try again");
+                }
+            } while (error);
+
+            Console.ReadKey();  
+        }
+    }
+}
