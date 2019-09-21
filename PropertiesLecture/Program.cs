7d89880624ae142a39af9531a02e9d433c31d0fb
Cornelia1991 <Cornelia1991@users.noreply.github.com>
2019-09-21 11:27:11 -0400

    Properties Lecture

---
+++ PropertiesLecture/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertiesLecture
+{
+    //protery is like a mixure between a field and a method
+    //its used like a field but calls a block of code
+
+    class MyClass
+    {
+        //short method
+        public int MyInteger { get; set; }
+
+        //private int _myInteger; //common pratice use "_" for field that will be used in property
+        
+        //public int MyInteger //The property || common practice same name as field w/o "_"
+        //{
+        //    // get and set are know as accessors
+        //    //if the proprety does not have a set accessor it is readonly
+        //    //if the proprety does not have a get accessor it is writeonly
+        //    get
+        //    {
+        //        return _myInteger;
+        //    }
+        //    set
+        //    {
+        //        _myInteger = value;
+        //    }
+        //}
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass MyObject = new MyClass();
+
+            MyObject.MyInteger = 5; // calls set accessor
+
+            Console.WriteLine(MyObject.MyInteger); //calls get accessor
+
+            Console.ReadKey();
+        }
+    }
+}
