a4fd1e5575270fe68aaba1c1734ac25ef03e7574
Cornelia1991 <Cornelia1991@users.noreply.github.com>
2019-09-21 09:49:32 -0400

    First commit debugging and error handling

---
+++ DebuggingLecture/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DebuggingLecture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = { 1, 2, 3};
+            int selection;
+
+            Console.Write("Eneter thae array index of the elementyou wish to view: ");
+            selection = Int32.Parse(Console.ReadLine());
+            Console.WriteLine(myArray[selection]);
+
+            Console.ReadKey();
+        }
+    }
+}
