01fac3125c1801015ce8283d250c5597614a5cf1
Cornelia1991 <Cornelia1991@users.noreply.github.com>
2019-09-21 10:14:03 -0400

    Manuel Error Handleing

--- DebuggingLecture/Program.cs
+++ DebuggingLecture/Program.cs
@@ -10,13 +10,45 @@ namespace DebuggingLecture
     {
         static void Main(string[] args)
         {
+
+            //Manuel Error Handling
+            //disadvantages: alot more code, only handles code that I am able to inticipate
+            //the code is less readable
+
+            //NEW KEYWORDS 
+            //TryParse: converts string to an int TryParse(String, Int32)
+            //out: used in TryParse(string s, out int result)
+            //Converts the string representation of a number to its 32-bit signed integer equivalent. 
+            //A return value indicates whether the conversion succeeded.
+
             int[] myArray = { 1, 2, 3};
-            int selection;
+            string selection;
+            int parsedInteger;
+            bool error = false;
 
-            Console.Write("Eneter thae array index of the elementyou wish to view: ");
-            selection = Int32.Parse(Console.ReadLine());
-            Console.WriteLine(myArray[selection]);
-
+            do
+            {
+                Console.Write("Eneter thae array index of the elementyou wish to view: ");
+                selection = Console.ReadLine();
+                if (int.TryParse(selection, out parsedInteger))
+                {
+                    if (parsedInteger >= 0 && parsedInteger <= myArray.Length - 1)
+                    {
+                        error = false;
+                        Console.WriteLine(myArray[parsedInteger]);
+                    }
+                    else
+                    {
+                        error = true;
+                        Console.WriteLine($"{selection} is not a valid index for the array. Please try again");
+                    }
+                }
+                else
+                {
+                    error = true;
+                    Console.WriteLine($"{selection} is not an integer. Please try again");
+                }
+            } while (error);
             Console.ReadKey();
         }
     }
